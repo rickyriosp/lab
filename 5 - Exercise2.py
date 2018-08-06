1a72204ce1bf1f5564699861e74dbcf3e96f7f39
nas7ybruises <riki.rios91@gmail.com>
2018-08-06 15:49:53 -0300

    Add 5 - Exercise2

---
+++ 5 - Exercise2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  6 15:39:15 2018
+
+@author: nas7ybruises
+"""
+
+import random
+def genEven():
+    '''
+    Returns a random even number x, where 0 <= x < 100
+    '''
+    return random.randrange(0, 100, 2)
