8fa2fb87c8778a3d9ea2a006dce272a2307d7287
rickyriosp <riki.rios91@gmail.com>
2020-07-27 23:06:00 -0400

    added layout components and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Logo from "./Components/Logo/Logo";
+import Navigation from "./Components/Navigation/Navigation";
+import Particles from "react-particles-js";
+import Rank from "./Components/Rank/Rank";
+import React from "react";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className="particles" params={particlesOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition />} */}
     </div>
   );
 }
