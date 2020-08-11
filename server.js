1131fd05179ad294b08e5c7e5f6fa665d11e268f
rickyriosp <riki.rios91@gmail.com>
2020-08-10 20:00:02 -0400

    update signin response

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("success");
+    res.json(database.users[0]);
   } else {
     res.status(404).json("error login in");
   }
