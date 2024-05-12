bb65d4691155c263628928ec08a437c8773062ab
rickyriosp <riki.rios91@gmail.com>
2024-05-11 23:48:42 -0400

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+
+    final String CRLF = "\r\n";
+    String httpOkResponse = "HTTP/1.1 200 OK" + CRLF;
+    String http404Response = "HTTP/1.1 404 Not Found" + CRLF;
+
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
@@ -20,9 +27,28 @@ public class Main {
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
 
-      String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+      InputStream clientInputStream  = clientSocket.getInputStream();
       OutputStream clientOutputStream = clientSocket.getOutputStream();
-      clientOutputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+
+      Scanner input = new Scanner(clientInputStream, StandardCharsets.UTF_8);
+      input.useDelimiter(CRLF);
+
+      String start = input.nextLine();
+      String method = start.split(" ")[0];
+      String path = start.split(" ")[1];
+      String version = start.split(" ")[2];
+
+      System.out.println("firstLine :: " + method + " " + path + " " + version);
+      System.out.println("client requesting connection to resource :: " + path);
+
+      switch (path) {
+        case "/":
+          clientOutputStream.write(httpOkResponse.getBytes(StandardCharsets.UTF_8));
+          break;
+        default:
+          clientOutputStream.write(http404Response.getBytes(StandardCharsets.UTF_8));
+      }
+
       clientOutputStream.flush();
       clientOutputStream.close();
     } catch (IOException e) {
