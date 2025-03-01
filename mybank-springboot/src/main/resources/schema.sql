3716193a6ed8ecea86d27dd0e5827db0a27008db
rickyriosp <riki.rios91@gmail.com>
2025-03-01 18:18:09 -0500

    .

--- mybank-springboot/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists transaction
-(
-    id uuid default random_uuid() primary key,
-    amount numeric not null,
-    time_stamp timestamp not null,
-    reference varchar(255) not null,
-    slogan varchar(255),
-    receiving_user varchar(255) not null
-);
